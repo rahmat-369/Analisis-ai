c52732f5444d62408cd0b5498157ce950859f75b
R_hmt dev <rohmatulloh.3609@gmail.com>
2026-02-25 04:12:29 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Analisis Daun AI",
+  description: "Deteksi penyakit daun dengan AI",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+    } 
